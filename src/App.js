414eb0dc58a1fd20bf06806cc264211e94298dda
GogogoDavis <burkedavis@yahoo.com>
2023-10-30 15:12:31 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'; 
+import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 
-const App = () => {
+function App() {
+  
+  const [recipeFormShown, showRecipeForm] = useState(false);
+
+  let submitRecipe = (event) => {};
+
   return (
-  <div className = 'App'>
-    <h1 className = "App Header">My Recipes</h1>
-    <p>There are no recipes to list</p>
-  </div>
-)}
+    <div className="App">
+      <h1 className="App-header">My Recipes</h1>
+      {
+        recipeFormShown ? 
+          <>
+            <form id="recipe-form" name='recipe-form' onSubmit={submitRecipe}>
+              <label htmlFor="newRecipeName">Recipe name: </label>
+              <input type="text" id="newRecipeName" />
+              <label htmlFor="newRecipeInstructions">Instructions:</label>
+              <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." />
+              <input type="submit" />
+            </form>
+          </>
+          : 
+          <>
+          <p>There are no recipes to list</p>
+          <button onClick={ () => showRecipeForm(!recipeFormShown) }>Add Recipe</button>
+          </>
+      }
+    </div>
+  );
+}
 
-export default App; 
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'; 
 
 test('As a chef I want a database for my recipes so that I may easily refer to them.', () => {
   render(<App />);
@@ -10,6 +11,35 @@ test('As a chef I want a database for my recipes so that I may easily refer to t
   let recipeList = screen.getByText('There are no recipes to list')
   expect(recipeList).toBeInTheDocument(); 
 
-  expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4); 
+  let button = screen.getByRole('button',{name: 'Add Recipe'}); 
 
+  expect(button).toBeInTheDocument(); 
+
+  expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4); 
+});
+
+test("contains an add recipe button that when clicked opens a form", async () => {
+  // render the landing page
+  render(<App />);
+
+  // wait for the page to load (implied, no async operations)
+
+  // click Add Recipe button
+  let button = screen.getByRole('button', {name: 'Add Recipe'});
+  userEvent.click(button);
+
+  // Wait for the form to appear on the screen (override the default of 1000ms as an example)
+  let form = await screen.findByRole('form', undefined, {timeout:3000});
+
+  // Verify the form appears
+  expect(form).toBeInTheDocument();
+
+  // Then I should see a form with fields: "Recipe Name" and "Recipe Instructions"
+  expect(screen.getByRole('textbox', {name: /Recipe name/i})).toBeInTheDocument();
+  expect(screen.getByRole('textbox', {name: /instructions/i})).toBeInTheDocument();
+  
+  // And the "Add Recipe" button should no longer be on the screen.
+  // Use queryBy instead of getBy because getBy throws an error when it doesn't have exactly 1 match
+  button = screen.queryByRole('button', {name: 'Add Recipe'});
+  expect(button).toBeNull();
 });
