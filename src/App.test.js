e3d6cb7fc56fb8f1f3b3601e558fb96963c3ec41
GogogoDavis <burkedavis@yahoo.com>
2023-10-30 14:20:20 -0600

    plz work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('As a chef I want a database for my recipes so that I may easily refer to them.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  let recipeHeader =  screen.getByText('My Recipes') 
+  expect(recipeHeader).toBeInTheDocument(); 
+
+  let recipeList = screen.getByText('There are no recipes to list')
+  expect(recipeList).toBeInTheDocument(); 
+
+  expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4); 
+
 });
